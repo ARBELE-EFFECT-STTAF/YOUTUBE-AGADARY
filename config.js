6633e5e317f8b25de97a27fb41a3eb0bcdb42ec3
PH-ARBELE11 <78043935+PH-ARBELE11@users.noreply.github.com>
2021-04-20 22:03:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "NzA1OTQ2MTQ0ODk4ODA5OTc4.XqzFlA.ZUnSTYfyZucelfdCDb16nvOIAow",
+    token: "",
     channel: "833853700803264555",
     messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
     channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
