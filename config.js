cfa96499914de45b512cb229408715a57dd7e37a
danyal91 <83440928+danyal91@users.noreply.github.com>
2021-05-26 13:48:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "",
-    channel: "833853700803264555",
+    token: "ODQ3MDU1OTgyMjAxNzk4NjY2.YK4gWw.CBDbxsyM9Mz_vjDWKbukHR3UBM4",
+    channel: "804824987143241812",
     messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
-    channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
+    channel_id: "3a-clQTmBkpxn817_SwbAg",
     watchInterval: 30000
 }
